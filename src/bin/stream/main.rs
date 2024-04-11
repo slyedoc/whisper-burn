4dd8793f345b7e6dddae129b6a36c1caa3dfed49
sudomonikers <sudomonikers@gmail.com>
2024-04-11 19:13:01 -0400

    it works!!! ish...

--- src/bin/stream/main.rs
+++ src/bin/stream/main.rs
@@ -1,70 +1,91 @@
-use std::collections::HashMap;
-use std::iter;
-
-use whisper::helper::*;
-use whisper::model;
-use whisper::model::*;
-use whisper::transcribe::waveform_to_text;
-use whisper::{token, token::Language};
-use hound::{self, SampleFormat};
 use burn::{
     config::Config,
     module::Module,
+    record::{DefaultRecorder, Recorder, RecorderError},
     tensor::{
         self,
         backend::{self, Backend},
         Data, Float, Int, Tensor,
     },
 };
-use strum::IntoEnumIterator;
+use burn_wgpu::{AutoGraphicsApi, WgpuBackend, WgpuDevice};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use hound::{self, SampleFormat};
 use num_traits::ToPrimitive;
-use whisper::audio::prep_audio;
-use whisper::token::{Gpt2Tokenizer, SpecialToken};
-use burn::record::{DefaultRecorder, Recorder, RecorderError};
-use std::{env, fs, process};
-use burn_wgpu::{WgpuBackend, WgpuDevice, AutoGraphicsApi};
+use anyhow::{Error as E, Result};
+use strum::IntoEnumIterator;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    env, fs, iter, process
+};
+use whisper::{
+    audio::prep_audio,
+    model::*,
+    helper::*,
+    token::{Gpt2Tokenizer, SpecialToken},
+    transcribe::waveform_to_text,
+    token, token::Language
+};
 
 //inference device backend
 type IDBackend = WgpuBackend<AutoGraphicsApi, f32, i32>;
-
 fn main() {
     //COMMAND LINE
-
-    let device = WgpuDevice::BestAvailable;
-
     let (model_name, wav_file, text_file, lang) = parse_args();
 
+    let device = WgpuDevice::BestAvailable;
     let (bpe, whisper_config, whisper) = load_model(&model_name, &device);
 
-
-
     //START AUDIO SERVER
+    // Set up the input device and stream with the default input config.
+    let audio_host = cpal::default_host();
+    let audio_device = audio_host
+        .default_input_device()
+        .expect("Failed to get default input device");
 
-    // //LOAD AUDIO
-    // println!("Loading waveform...");
-    // let (waveform, sample_rate) = match load_audio_waveform::<IDBackend>(&wav_file) {
-    //     Ok((w, sr)) => (w, sr),
-    //     Err(e) => {
-    //         eprintln!("Failed to load audio file: {}", e);
-    //         process::exit(1);
-    //     }
-    // };
+    let audio_config = audio_device
+        .default_input_config()
+        .expect("Failed to get default input config");
 
-    // //RUN INFERENCE
-    // let (text, tokens) = match waveform_to_text(&whisper, &bpe, lang, waveform, sample_rate) {
-    //     Ok((text, tokens)) => (text, tokens),
-    //     Err(e) => {
-    //         eprintln!("Error during transcription: {}", e);
-    //         process::exit(1);
-    //     }
-    // };
+    let channel_count = audio_config.channels() as usize;
 
-    // fs::write(text_file, text).unwrap_or_else(|e| {
-    //     eprintln!("Error writing transcription file: {}", e);
-    //     process::exit(1);
-    // });
+    let audio_ring_buffer = Arc::new(Mutex::new(Vec::new()));
+    let audio_ring_buffer_2 = audio_ring_buffer.clone();
 
-    println!("Transcription finished.");
+    std::thread::spawn(move || loop {
+        let data = record_audio(&audio_device, &audio_config, 300).unwrap();
+        audio_ring_buffer.lock().unwrap().extend_from_slice(&data);
+        let max_len = data.len() * 16;
+        let data_len = data.len();
+        let len = audio_ring_buffer.lock().unwrap().len();
+        if len > max_len {
+            let mut data = audio_ring_buffer.lock().unwrap();
+            let new_data = data[data_len..].to_vec();
+            *data = new_data;
+        }
+    });
+
+    // loop to process the audio data forever (until the user stops the program)
+    println!("Transcribing audio...");
+    for (i, _) in iter::repeat(()).enumerate() {
+        std::thread::sleep(std::time::Duration::from_millis(3000));
+        let data = audio_ring_buffer_2.lock().unwrap().clone();
+        let pcm_data: Vec<_> = data[..data.len() / channel_count as usize]
+            .iter()
+            .map(|v| *v as f32 / 32768.)
+            .collect();
+
+        //RUN INFERENCE
+        let (text, tokens) = match waveform_to_text(&whisper, &bpe, lang, pcm_data, 16000) {
+            Ok((text, tokens)) => (text, tokens),
+            Err(e) => {
+                eprintln!("Error during transcription: {}", e);
+                process::exit(1);
+            }
+        };
+        println!("{:?}", text);
+    }
 }
 
 fn parse_args() -> (String, String, String, Language) {
@@ -94,32 +115,6 @@ fn parse_args() -> (String, String, String, Language) {
     (model_name, wav_file, text_file, lang)
 }
 
-fn load_audio_waveform<B: Backend>(filename: &str) -> hound::Result<(Vec<f32>, usize)> {
-    let mut reader = hound::WavReader::open(filename)?;
-    let spec = reader.spec();
-
-    let duration = reader.duration() as usize;
-    let channels = spec.channels as usize;
-    let sample_rate = spec.sample_rate as usize;
-    let bits_per_sample = spec.bits_per_sample;
-    let sample_format = spec.sample_format;
-
-    assert_eq!(sample_rate, 16000, "The audio sample rate must be 16k.");
-    assert_eq!(channels, 1, "The audio must be single-channel.");
-
-    let max_int_val = 2_u32.pow(spec.bits_per_sample as u32 - 1) - 1;
-
-    let floats = match sample_format {
-        SampleFormat::Float => reader.into_samples::<f32>().collect::<hound::Result<_>>()?,
-        SampleFormat::Int => reader
-            .into_samples::<i32>()
-            .map(|s| s.map(|s| s as f32 / max_int_val as f32))
-            .collect::<hound::Result<_>>()?,
-    };
-
-    return Ok((floats, sample_rate));
-}
-
 fn load_whisper_model_file<B: Backend>(
     config: &WhisperConfig,
     model_name: &str,
@@ -129,7 +124,10 @@ fn load_whisper_model_file<B: Backend>(
         .map(|record| config.init().load_record(record))
 }
 
-fn load_model(model_name: &str, device: &WgpuDevice) -> (Gpt2Tokenizer, WhisperConfig, Whisper<IDBackend>) {
+fn load_model(
+    model_name: &str,
+    device: &WgpuDevice,
+) -> (Gpt2Tokenizer, WhisperConfig, Whisper<IDBackend>) {
     let bpe = match Gpt2Tokenizer::new(&model_name) {
         Ok(bpe) => bpe,
         Err(e) => {
@@ -159,4 +157,35 @@ fn load_model(model_name: &str, device: &WgpuDevice) -> (Gpt2Tokenizer, WhisperC
     let whisper = whisper.to_device(&device);
 
     (bpe, whisper_config, whisper)
+}
+
+fn record_audio(
+    device: &cpal::Device,
+    config: &cpal::SupportedStreamConfig,
+    milliseconds: u64,
+) -> Result<Vec<i16>> {
+    let writer = Arc::new(Mutex::new(Vec::new()));
+    let writer_2 = writer.clone();
+    let stream = device.build_input_stream(
+        &config.config(),
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            let processed = data
+                .iter()
+                .map(|v| (v * 32768.0) as i16)
+                .collect::<Vec<i16>>();
+            writer_2.lock().unwrap().extend_from_slice(&processed);
+        },
+        move |err| {
+            eprintln!("an error occurred on stream: {}", err);
+        },
+        None,
+    )?;
+    stream.play()?;
+    std::thread::sleep(std::time::Duration::from_millis(milliseconds));
+    drop(stream);
+    let data = writer.lock().unwrap().clone();
+    let step = 3;
+    let data: Vec<i16> = data.iter().step_by(step).copied().collect();
+    //println!("{:?}", data);
+    Ok(data)
 }
